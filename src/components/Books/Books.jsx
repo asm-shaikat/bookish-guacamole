64d96460f8118a81635a4ae038a4b0d385df576e
asm-shaikat <asmshaikat1952@gmail.com>
2024-10-02 00:14:01 +0600

    hero section added and Book card added

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,209 @@
+import React from "react";
+
+const Books = () => {
+  return (
+    <div className="mt-8">
+      <div>
+        <p className="text-2xl md:text-3xl text-black font-semibold mb-6">Books</p>
+
+        <div className="grid grid-cols-3 sm:flex-row gap-4">
+          
+          {/* Book Card */}
+          <div className="bg-white border border-gray-200 rounded-xl p-4 sm:p-6 md:p-8 w-full sm:w-96">
+            <div className="card bg-gray-100 shadow-xl">
+              <figure className="">
+                <img
+                  src="/public/Assets/book1.png"
+                  alt="Book Cover"
+                  className="w-32 h-44 p-4 mt-4" // Adjusted image width and height
+                />
+              </figure>
+
+              <div className="card-body bg-white text-black">
+                {/* Badges */}
+                <div className="flex flex-wrap gap-2 mb-2">
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Young Adult
+                  </span>
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Identity
+                  </span>
+                </div>
+
+                {/* Title */}
+                <h2 className="card-title mb-1 text-lg sm:text-xl">
+                  The Catcher in the Rye
+                </h2>
+
+                {/* Author */}
+                <p className="text-sm text-left text-gray-600 mb-2">
+                  By: Awlad Hossain
+                </p>
+
+                <div className="border border-dashed"></div>
+
+
+                {/* Footer */}
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-sm font-semibold">Fiction</span>
+                  <div className="flex items-center">
+                    <p className="mr-1 text-sm font-semibold">5.00</p>
+                    <span className="text-xs text-yellow-500">★</span> {/* Star rating */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Book card end */}
+
+          {/* Book Card */}
+          <div className="bg-white border border-gray-200 rounded-xl p-4 sm:p-6 md:p-8 w-full sm:w-96">
+            <div className="card bg-gray-100 shadow-xl">
+              <figure className="">
+                <img
+                  src="/public/Assets/book1.png"
+                  alt="Book Cover"
+                  className="w-32 h-44 p-4 mt-4" // Adjusted image width and height
+                />
+              </figure>
+
+              <div className="card-body bg-white text-black">
+                {/* Badges */}
+                <div className="flex flex-wrap gap-2 mb-2">
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Young Adult
+                  </span>
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Identity
+                  </span>
+                </div>
+
+                {/* Title */}
+                <h2 className="card-title mb-1 text-lg sm:text-xl">
+                  The Catcher in the Rye
+                </h2>
+
+                {/* Author */}
+                <p className="text-sm text-left text-gray-600 mb-2">
+                  By: Awlad Hossain
+                </p>
+
+                <div className="border border-dashed"></div>
+
+
+                {/* Footer */}
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-sm font-semibold">Fiction</span>
+                  <div className="flex items-center">
+                    <p className="mr-1 text-sm font-semibold">5.00</p>
+                    <span className="text-xs text-yellow-500">★</span> {/* Star rating */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Book card end */}
+
+          {/* Book Card */}
+          <div className="bg-white border border-gray-200 rounded-xl p-4 sm:p-6 md:p-8 w-full sm:w-96">
+            <div className="card bg-gray-100 shadow-xl">
+              <figure className="">
+                <img
+                  src="/public/Assets/book1.png"
+                  alt="Book Cover"
+                  className="w-32 h-44 p-4 mt-4" // Adjusted image width and height
+                />
+              </figure>
+
+              <div className="card-body bg-white text-black">
+                {/* Badges */}
+                <div className="flex flex-wrap gap-2 mb-2">
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Young Adult
+                  </span>
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Identity
+                  </span>
+                </div>
+
+                {/* Title */}
+                <h2 className="card-title mb-1 text-lg sm:text-xl">
+                  The Catcher in the Rye
+                </h2>
+
+                {/* Author */}
+                <p className="text-sm text-left text-gray-600 mb-2">
+                  By: Awlad Hossain
+                </p>
+
+                <div className="border border-dashed"></div>
+
+
+                {/* Footer */}
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-sm font-semibold">Fiction</span>
+                  <div className="flex items-center">
+                    <p className="mr-1 text-sm font-semibold">5.00</p>
+                    <span className="text-xs text-yellow-500">★</span> {/* Star rating */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Book card end */}
+
+
+          {/* Book Card */}
+          <div className="bg-white border border-gray-200 rounded-xl p-4 sm:p-6 md:p-8 w-full sm:w-96">
+            <div className="card bg-gray-100 shadow-xl">
+              <figure className="">
+                <img
+                  src="/public/Assets/book1.png"
+                  alt="Book Cover"
+                  className="w-32 h-44 p-4 mt-4" // Adjusted image width and height
+                />
+              </figure>
+
+              <div className="card-body bg-white text-black">
+                {/* Badges */}
+                <div className="flex flex-wrap gap-2 mb-2">
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Young Adult
+                  </span>
+                  <span className="badge bg-gray-100 border border-gray-100 font-semibold text-green-400">
+                    Identity
+                  </span>
+                </div>
+
+                {/* Title */}
+                <h2 className="card-title mb-1 text-lg sm:text-xl">
+                  The Catcher in the Rye
+                </h2>
+
+                {/* Author */}
+                <p className="text-sm text-left text-gray-600 mb-2">
+                  By: Awlad Hossain
+                </p>
+
+                <div className="border border-dashed"></div>
+
+                {/* Footer */}
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-sm font-semibold">Fiction</span>
+                  <div className="flex items-center">
+                    <p className="mr-1 text-sm font-semibold">5.00</p>
+                    <span className="text-xs text-yellow-500">★</span> {/* Star rating */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Book card end */}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import Books from "../Books/Books";
 
 const Home = () => {
     return (
-        <div className=' bg-white text-black h-screen'>
-            <p>Welcome Home</p>
+      <>
+        <div className="bg-slate-200 text-black mt-8 p-8 md:p-14 rounded-2xl">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="flex flex-col justify-center items-center w-full md:w-2/4">
+            <div className="text-center w-full md:w-3/4">
+              <p className="text-2xl md:text-4xl">
+                Books to freshen up your bookshelf
+              </p>
+            </div>
+            <div className="mt-4 md:mt-2">
+              <button className="btn btn-success text-white m-2">
+                View The List
+              </button>
+            </div>
+          </div>
+          <div className="w-full md:w-2/4 flex justify-center md:justify-end mt-4 md:mt-0">
+            <img
+              className="max-w-full h-auto"
+              src="/public/Assets/book-3d.png"
+              alt="3D Book"
+            />
+          </div>
         </div>
+      </div>
+      <Books></Books>
+      </>
     );
-};
-
-export default Home;
+  };
+  
+  export default Home;
+  
